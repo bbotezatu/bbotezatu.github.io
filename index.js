e3152a78f0742574fe91582876250d8b47a05c32
Brandon Botezatu <decadeofdefiance@gmail.com>
2019-10-25 12:33:50 -0500

    stuff

---
+++ index.js
@@ -0,0 +1,8 @@
+/*document.head.classList.add('image-loading');
+window.addEventListener("load", displayPage)
+
+function displayPage(){
+    document.head.classList.remove('image-loading');
+}
+*/
+//...this probably doesn't even do anything
